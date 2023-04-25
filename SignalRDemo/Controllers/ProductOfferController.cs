ac3c0ea07f3e376439db48eff3eb755c949693d5
Josh-Perez <joshua_perez@goodyear.com>
2023-04-26 00:23:05 +0800

    Upload project to GitHub

---
+++ SignalRDemo/Controllers/ProductOfferController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemo.Hub;
+
+namespace SignalRDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductOfferController : ControllerBase
+    {
+        private IHubContext<MessageHub, IMessageHubClient> messageHub;
+        public ProductOfferController(IHubContext<MessageHub, IMessageHubClient> _messageHub)
+        {
+            this.messageHub = _messageHub;
+        }
+
+        [HttpPost]
+        [Route("productoffers")]
+        public string Get()
+        {
+            List<string> offers = new List<string>();
+            offers.Add("20% Off on Iphone 12");
+            offers.Add("15% Off on HP Pavillion");
+            offers.Add("10% Off on Samsung Galaxy S20");
+
+            messageHub.Clients.All.SendOffersToUser(offers);
+
+            return "Offers sent to all users!";
+        }
+    }
+}
