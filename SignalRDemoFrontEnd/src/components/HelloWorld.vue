4755dccf47d0c2ef7c7480e0603e4326c772dcb7
Josh-Perez <joshua_perez@goodyear.com>
2023-04-26 01:11:56 +0800

    Adding a client that listens to the SignalR server hub

--- SignalRDemoFrontEnd/src/components/HelloWorld.vue
+++ SignalRDemoFrontEnd/src/components/HelloWorld.vue
@@ -1,36 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
-defineProps({
-  msg: String,
-})
+const offers = ref([]);
 
-const count = ref(0)
+onMounted(() => {
+  const hubConnectionBuilder = new HubConnectionBuilder().withUrl('https://localhost:7039/offers')
+  .configureLogging(LogLevel.Information).build();
+
+  hubConnectionBuilder.start().then(() => {
+    console.log('Connection started.....!');
+  }).catch(err => console.log('Error while starting connection: ' + err));
+
+  hubConnectionBuilder.on('SendOffersToUser', (result) => {
+    offers.value.push(result);
+  });
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div v-if="offers.length > 0">
+    <ul>
+      <li v-for="(offer, index) in offers">
+        Offers: <b>{{ offer.join(', ') }}</b>
+      </li>
+    </ul>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
